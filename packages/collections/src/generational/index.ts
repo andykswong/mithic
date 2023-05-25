28b49092af9ebc8b42f7358b283936bfb7ec9f98
Andy K. S. Wong <andykswong@outlook.com>
2023-05-25 11:31:04 +0800

    Fix imports

--- packages/collections/src/generational/index.ts
+++ packages/collections/src/generational/index.ts
@@ -1,3 +1,6 @@
-export * as GenerationalId from './id.js';
+export * from './arena.js';
+export * from './generator.js';
 export * from './genidmap.js';
 export * from './sparseset.js';
+
+export * as GenerationalId from './id.js';
